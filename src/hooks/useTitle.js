cbf50a63b660fa571f98bfb325db9e13f20b9a89
Luis Ruiz <l.ruiz5@live.com>
2023-03-21 11:18:39 -0700

    Update useTitle.js

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export const useTitle = (title) => {
   useEffect(() => {
-    document.title = `Luis / ${title || "Loading..."}`;
+    document.title = `${title || "Loading..."} / Luis`;
   }, [title]);
   return null;
 };
